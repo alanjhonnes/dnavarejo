8e4657f356a63e29701b9b59cf6f798bee1fd8d4
Alan Jhonnes <aj@alanjhonnes.com>
2014-05-05 10:28:49 -0300

    Frontend work, animations

--- web/js/compiled/main.js
+++ web/js/compiled/main.js
@@ -2297,6 +2297,8 @@ if ( typeof module != 'undefined' && module.exports ) {
 		}
 	};
 })(jQuery);
+var controller;
+
 $(document).ready(function(){
     
     var $header = $('header');
@@ -2307,14 +2309,15 @@ $(document).ready(function(){
     
     
     var $buttons = $("nav a");
-    var $sections = $("section");
+    var $sections = $(".primary-section");
     var $activeButton = $buttons.eq(0);
     var $activeSection = $sections.eq(0);
     var activeIndex = 0;
     var scrollpoints = [];
     var navHeight = 0;
     
-    var colors = ['#3D4D62', '#5E7082', '#657F94', '#7897AC', '#4F8AA8', '#85ABBC', '#697AA9' ];
+    //var colors = ['#3D4D62', '#5E7082', '#657F94', '#7897AC', '#4F8AA8', '#85ABBC', '#697AA9' ];
+    var colors = ['#3D4D62', '#5E7082', '#657F94', '#7897AC', '#697AA9' ];
     
     $activeButton.addClass("active-button");
     
@@ -2380,7 +2383,7 @@ $(document).ready(function(){
         $(this).addClass('active-button');
         var sectionIndex = $(this).index() -1;
         //scroll down to the respective section
-        var yOffset = $("section:eq(" + sectionIndex + ")").offset().top - navHeight;
+        var yOffset = $(".primary-section:eq(" + sectionIndex + ")").offset().top - navHeight;
         TweenLite.to(window, 0.6, { scrollTo: { y: yOffset } } );
     });
     
@@ -2388,18 +2391,152 @@ $(document).ready(function(){
        $(this).toggleClass("open").prev().fadeToggle("fast");
     });
     
+    var $casePage = $("#case-page");
+    var $caseClose = $("#case-close");
+    $("#clients-cases li").click(function(){
+       $casePage.css("display","block");
+       TweenMax.to($casePage, 0.7, { x: 10, opacity: 1 } ); 
+    });
     
-    var tween = TweenMax.to("#animate1", 0.5, {backgroundColor: "green", scale: 2.5});
+    $caseClose.click(function(){
+        TweenMax.to($casePage, 0.7, { x: "100%", opacity: 0, onComplete: function(){
+                $casePage.css("display","none");
+        }} ); 
+    });
+    
+    
+    controller = new ScrollMagic();
+    
+    var aboutTimeline = new TimelineMax();
+    aboutTimeline.add(new TweenMax.staggerFrom($("#white-bar div"), 0.7, { opacity: 0, y: "-=50" }, 0.2 ));
 
     // build scene
-    var scene = new ScrollScene({triggerElement: "#trigger1"})
-                                    .setTween(tween)
+    var aboutScene = new ScrollScene({triggerElement: "#about", triggerHook: "onCenter"})
+                                    .setTween(aboutTimeline)
                                     .addTo(controller);
-
+                            
+                            
+    var workTimeline = new TimelineMax();
+    
+    //index of icons that should have their labels animated to the left;
+    var labelsToLeft = [0, 1, 4, 5];
+    
+    
+    $("#work article").each(function(index){
+        var $icon = $(this).children(".icon");
+        var $label = $(this).children(".icon-label");
+        var $arrow = $(this).next();
+        var $plus = $(this).children(".plus-button");
+        workTimeline.add(TweenMax.from($icon, 0.7, { opacity: 0, scale: 1.4 }), "-=0.55");
+        //animate from right to left
+        if($plus){
+            workTimeline.add(TweenMax.from($plus, 0.7, { opacity: 0, scale: 1.4 }), "-=0.55");
+        }
+        workTimeline.add(TweenMax.from($arrow, 0.7, { opacity: 0, scale: 1.4 }), "-=0.55");
+        if(labelsToLeft.indexOf(index) !== -1){
+            workTimeline.add(TweenMax.from($label, 0.7, { opacity: 0, x: "+= 80" }), "-=0.55");
+        }
+        //animate from left to right
+        else {
+            workTimeline.add(TweenMax.from($label, 0.7, { opacity: 0, x: "-= 80" }), "-=0.55");
+        }
+        
+    });
+    
+    // build scene
+    var workScene = new ScrollScene({triggerElement: "#work", triggerHook: 0.2})
+                                    .setTween(workTimeline)
+                                    .addTo(controller);
+    
+    var servicesTimeline = new TimelineMax();
+    
+    $("#services .girl-icon").each(function(){
+        var $ballon = $(this).children("div");
+        servicesTimeline.add(TweenMax.from($(this), 0.7, { opacity: 0, scale: 1.4 }), "-=0.55");
+        servicesTimeline.add(TweenMax.from($ballon, 0.7, { opacity: 0, y:"-=50" }), "-=0.55");
+    });
+    
+    var $greenArrows = $("#services .responsive-green-arrow");
+    
+    servicesTimeline.add(TweenMax.from($greenArrows.first(), 
+                                        0.7, { opacity: 0, x:"-=50", scale: 1.4 }), "-=0.55");
+    
+    servicesTimeline.add(TweenMax.from($("#services .resume"), 
+                                        0.7, { opacity: 0, y:"-=50", scale: 1.4 }), "-=0.25");
+    
+    servicesTimeline.add(TweenMax.from($greenArrows.last(), 
+                                        0.7, { opacity: 0, x:"-=50", scale: 1.4 }), "-=0.55");
+                                        
+    servicesTimeline.add(TweenMax.from($("#services .apples"), 
+                                        0.7, { opacity: 0, x:"-=50" }), "-=0.25");
+                                        
+    servicesTimeline.add(TweenMax.from($("#services .girl"), 
+                                        0.7, { opacity: 0, y:"-=50" }), "-=0.35");
+                                        
+    servicesTimeline.add(TweenMax.allFrom($("#services-list > div"), 
+                                        0.7, { opacity: 0, x:"-=50" }, 0.3), "-=0.35");
+    
+    var servicesScene = new ScrollScene({triggerElement: "#services", triggerHook: 0.2})
+                                    .setTween(servicesTimeline)
+                                    .addTo(controller);
+                            
+                            
+    var recruitmentTimeline = new TimelineMax();
+    
+    recruitmentTimeline.add(TweenMax.staggerFrom($("#recruitment-icons div"), 0.7, {opacity: 0, scale: 1.4 }, 0.2));
+    
+    var recruitmentScene = new ScrollScene({triggerElement: "#recruitment", triggerHook: 0.2})
+                                    .setTween(recruitmentTimeline)
+                                    .addTo(controller);
+                            
+    var trainningTimeline = new TimelineMax();
+    
+    trainningTimeline.add(TweenMax.staggerFrom($("#trainning-icons div"), 0.7, {opacity: 0, scale: 1.4 }, 0.2));
+    
+    var trainningScene = new ScrollScene({triggerElement: "#trainning", triggerHook: 0.2})
+                                    .setTween(trainningTimeline)
+                                    .addTo(controller);
+                            
+    var clientsTimeline = new TimelineMax();
+    
+    $(".client-icon").each(function(index){
+        var $icon = $(this).children(".icon");
+        var $clientCheckmark = $(this).children(".checkmark-green");
+        var $clientLabel = $(this).children(".icon-label");
+        
+        clientsTimeline.add(TweenMax.from($icon, 0.7, { opacity: 0, x:"-=50", scale: 1.4 }), "-=0.5");
+        clientsTimeline.add(TweenMax.from($clientCheckmark, 0.7, { opacity: 0, y:"-=50", scale: 1.4 }), "-=0.5");
+        clientsTimeline.add(TweenMax.from($clientLabel, 0.7, { opacity: 0, y:"-=50", scale: 1.4 }), "-=0.5");
+        
+        //se indice for menos que 2 dá pra buscar a proxima seta a seguir
+        if(index < 2){
+            var $arrow = $(this).next();
+            clientsTimeline.add(TweenMax.from($arrow, 0.7, { opacity: 0, x:"-=50", scale: 1.4 }), "-=0.5");
+        }
+        
+    });
+    
+    var clientsScene = new ScrollScene({triggerElement: "#clients", triggerHook: 0.5})
+                                    .setTween(clientsTimeline)
+                                    .addTo(controller);
+                            
+    var contactTimeline = new TimelineMax();
+    
+    $("#contact-icons div").each(function(){
+        contactTimeline.add(TweenMax.from($(this), 0.7, { opacity: 0, x:"-=50", scale: 1.4 }), "-=0.5");
+        
+    });
+    
+    var contactScene = new ScrollScene({triggerElement: "#contact", triggerHook: 0.2})
+                                    .setTween(contactTimeline)
+                                    .addTo(controller);
+    
     // show indicators (requires debug extension)
-    scene.addIndicators();
-    
-    
-    
-    
+//    aboutScene.addIndicators();
+//    workScene.addIndicators();
+//    servicesScene.addIndicators();
+//    recruitmentScene.addIndicators();
+//    trainningScene.addIndicators();
+//    clientsScene.addIndicators();
+//    contactScene.addIndicators();
 });
